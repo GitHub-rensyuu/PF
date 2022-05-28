9be2d63de3efaf15e6dd65a858cb089ca17accfa
RT <u7def37decdwd@gmail.com>
2022-05-28 11:05:09 +0000

    コメント削除時のエラー修正

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -7,7 +7,17 @@ class Admin::CommentsController < ApplicationController
 
   def destroy
     comment = Comment.find(params[:id])
+    source = comment.source
     comment.destroy
+    if source.comments.count == 0
+      total_rate = source.rate
+      total_recommended_rank = source.recommended_rank 
+    else
+      total_rate = source.comments.average(:rate)
+      total_recommended_rank = source.comments.average(:recommended_rank).round
+    end
+    source.update(total_rate: total_rate)
+    source.update(total_recommended_rank: total_recommended_rank)
     redirect_to request.referer
   end
 end
