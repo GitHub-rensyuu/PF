7f3a49eabafea027db9470abaa299307833844dc
RT <u7def37decdwd@gmail.com>
2022-05-28 10:18:11 +0000

    30日後自動削除

--- lib/batch/data_reset.rb
+++ lib/batch/data_reset.rb
@@ -2,7 +2,7 @@ class Batch::DataReset
   def self.data_reset
     # 投稿を全て削除
     if Notice.where("created_at < ?", 30.days.ago).present?
-      Notice.where("created_at < ?", 30.days.ago).delete_allif 
+      Notice.where("created_at < ?", 30.days.ago).delete_all
       p "30日前の通知を全て削除しました"
     end
   end
