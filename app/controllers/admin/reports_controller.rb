f173fef21d78f3afad55b61af95d3dc24ee939b3
RT <u7def37decdwd@gmail.com>
2022-06-10 13:06:11 +0000

    不要ファイル削除

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,5 +1,13 @@
 class Admin::ReportsController < ApplicationController
+  
   def destroy
+    reporting = current_customer.unreport(params[:customer_id])
+    flash[:notice] = if reporting.destroy
+                       'ユーザーの通報を解除しました'
+                     else
+                       'ユーザーの通報解除に失敗しました'
+                     end
+    redirect_to request.referer
   end
 
 end
